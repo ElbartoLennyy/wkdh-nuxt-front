85d093700cdadfc1ee5e8986f24426882d0e2c0a
Alexander Gerick <Elbarto2002@gmail.com>
2020-04-18 11:49:01 +0200

    generate possible start times

--- server/lib/pickUp.js
+++ server/lib/pickUp.js
@@ -3,6 +3,12 @@ const Distance = require('geo-distance')
 const firebase = require('./firebase')
 const geocoder = require('./geocoder')
 
+checkPickUp({
+  Adress: 'Azaleenweg 11',
+  PLZ: ' 02827',
+  City: 'Görlitz',
+})
+
 async function checkPickUp(userLocation) {
   const userGeoLocation = await geocoder.validateAddress(userLocation)
 
@@ -28,13 +34,55 @@ async function checkPickUp(userLocation) {
   avaibleCouriers.sort((a, b) => {
     return a.location.distance - b.location.distance
   })
-  console.log(util.inspect(avaibleCouriers, false, null, true /* enable colors */))
-
   if (avaibleCouriers.length === 0) {
     return { location: userGeoLocation, pickUpData: false }
   } else if (avaibleCouriers.length >= 1) {
+    generateAllPossibleTimes(avaibleCouriers)
     return { location: userGeoLocation, pickUpData: avaibleCouriers }
   }
+}
+
+function generateAllPossibleTimes(times) {
+  console.log(util.inspect(times, false, null, true /* enable colors */))
+
+  const startTimes = {}
+
+  for (const courierTimes of times) {
+    for (const day in courierTimes.pickUpTimes) {
+      for (const slot of courierTimes.pickUpTimes[day]) {
+        const starts = []
+        const startHour = new Date(slot.start).getHours()
+        const startMinute = new Date(slot.start).getMinutes()
+        const endHour = new Date(slot.end).getHours()
+        const endMinute = new Date(slot.end).getMinutes()
+
+        for (let h = startHour; h < endHour; h++) {
+          const exclude00 = h === startHour && startMinute === 30
+          if (!exclude00) {
+            starts.push([h, 0])
+          }
+
+          const exclude30 = h === (endHour - 1) && endMinute !== 30
+          if (!exclude30) {
+            starts.push([h, 30])
+          }
+        }
+
+        for (const start in starts) {
+          starts[start].push(courierTimes.cId)
+        }
+
+        if (startTimes[day] === undefined) {
+          startTimes[day] = starts
+        } else {
+          for (const times of startTimes[day]) {
+            console.log(times)
+          }
+        }
+      }
+    }
+  }
+  // console.log(util.inspect(startTimes, false, null, true /* enable colors */))
 }
 
 module.exports = { checkPickUp }
