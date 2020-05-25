3cb4818aaf659a9029ca5e7f24f29dfd6a3d4b27
Alexander Gerick PC <Elbarto2002@gmail.com>
2020-05-25 09:05:00 +0200

    starting new design

---
+++ pages/new-index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="min-h-screen min-w-full font-sans">
+    <div class="m-8 flex h-24">
+      <div class="w-1/4">
+        <img class="w-full h-auto max-h-full object-contain" src="~assets/img/icons/Logo-new-1000.png" alt="">
+      </div>
+      <div class="w-3/4">
+        dwad
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
     extend: {
       colors: {
         buttonBlue: '#02baff',
+        yellowLight: '#ffbb39',
+        yellowDark: '#faa401',
       },
       scale: {
         99: '.99',
         98: '.98',
       },
     },
+  },
+  fontFamily: {
+    sans: ['Roboto'],
   },
   variants: ['responsive', 'group-hover', 'focus-within', 'first', 'last', 'odd', 'even', 'hover', 'focus', 'active', 'visited', 'disabled'],
   plugins: [],
