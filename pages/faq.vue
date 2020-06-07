c33712c97d9d65ed094fd8af85b6c5217ee3613a
mariusmarx <m.marx2103@gmail.com>
2020-06-07 12:26:40 +0200

    Update FAQ

--- pages/faq.vue
+++ pages/faq.vue
@@ -43,15 +43,27 @@
       </div>
     </div>
     <div class="bg-gradient w-full text-center">
-      <p class="text-3xl text-white font-bold tracking-wide p-6">
+      <p class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white font-bold tracking-wide p-6">
         Willkommen im Wirkaufendeinhandy Hilfe-Center
       </p>
     </div>
-    <div class="grid grid-cols-4 gap-4">
-<div></div>
-<div></div>
-<div></div>
-<div></div>
+     <div class="grid grid-cols-4 gap-20 px-20 py-12 text-center text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">
+      <div class="border-gray-500 border-2 rounded-lg">
+        <img class="h-12 xl:h-20 mx-auto m-8" src="~assets/img/svg/Versand-Lieferstatus.svg" alt="">
+        <p class="mb-8">Versand & Lieferstatus</p>
+      </div>
+      <div class="border-gray-500 border-2 rounded-lg">
+        <img class="h-12 xl:h-20 mx-auto m-8" src="~assets/img/svg/Verkaufen-Bewertung.svg" alt="">
+        <p class="mb-8">Verkaufen & Bewertung</p>
+      </div>
+      <div class="border-gray-500 border-2 rounded-lg">
+        <img class="h-12 xl:h-20 mx-auto m-8" src="~assets/img/svg/Scherpe orange.svg" alt="">
+        <p class="mb-8">Unsere Preisgarantie</p>
+      </div>
+      <div class="border-gray-500 border-2 rounded-lg">
+        <img class="h-12 xl:h-20 mx-auto m-8" src="~assets/img/svg/Auszahlung-Icon.svg" alt="">
+        <p class="mb-8">Auszahlung</p>
+      </div>
     </div>
     <footerTemplate />
   </div>
